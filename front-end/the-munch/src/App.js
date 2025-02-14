c0af97853ba33d9873f1cd280cf83e41e49d5b07
Joseph Kennedy <joekentafir@gmail.com>
2025-02-15 01:15:10 +0300

    adds Navbar on App component

--- front-end/the-munch/src/App.js
+++ front-end/the-munch/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>The Munch</h1>
-      </header>
+      <Navbar />
     </div>
   );
 }
---
+++ front-end/the-munch/src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='Navbar'>
+      <h1>The Munch</h1>
+      
+    </div>
+  )
+}
+
+export default Navbar
