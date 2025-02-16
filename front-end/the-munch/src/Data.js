269813440b344cd218ca6c97a0202b5258fe3319
Joseph Kennedy <joekentafir@gmail.com>
2025-02-16 14:09:47 +0300

    adds arrays of objects

--- front-end/the-munch/src/Data.js
+++ front-end/the-munch/src/Data.js
@@ -1,5 +1,16 @@
 export const formData = [
   {
-    
-  }
+    id:1,
+    itemName:"",
+    category:"",
+    price:"",
+    description:""
+  },
+  {
+    id:2,
+    itemName:"",
+    category:"",
+    price:"",
+    description:""
+  },
 ]
