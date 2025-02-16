79d6fc08b8aba35231675804b2ea7d281fc37ef2
Joseph Kennedy <joekentafir@gmail.com>
2025-02-16 15:03:47 +0300

    adds items to my arrays

--- front-end/the-munch/src/Data.js
+++ front-end/the-munch/src/Data.js
@@ -1,30 +1,38 @@
 export const formData = [
   {
     id:1,
-    itemName:"",
-    category:"",
-    price:"",
-    description:""
+    image:"",
+    itemName:"Classic Beef Burger",
+    category:"Burgers",
+    price:700,
+    description:"Buns, Beef Patty, Onions, Tomatoes, Chadder cheese, Ketchup, Mayonasse, Mustard aioli, Lettuce, Bacon, Egg.",
+    extras:"Fries"
   },
   {
     id:2,
-    itemName:"",
-    category:"",
-    price:"",
-    description:""
+    image:"",
+    itemName:"Classic Double Beef Burger",
+    category:"Burgers",
+    price:800,
+    description:"Buns, Aged Beef Patty(2), Onions, Tomatoes, Chadder cheese, Ketchup, Mayonasse, Mustard aioli, Lettuce, Bacon, Egg.",
+    extras:"Fries"
   },
   {
     id:3,
-    itemName:"",
-    category:"",
-    price:"",
-    description:""
+    image:"",
+    itemName:"Chicken Burger",
+    category:"Burgers",
+    price:750,
+    description:"Buns, Marinated Chicken Breast, Caramalized Onions, Tomatoes, Chadder cheese, Ketchup aioli, Garlic aioli, Mustard aioli, Lettuce, Egg.",
+    extras:"Fries"
   },
   {
     id:4,
-    itemName:"",
-    category:"",
-    price:"",
-    description:""
+    image:"",
+    itemName:"Double Chicken Burger",
+    category:"Burgers",
+    price:950,
+    description:"Buns, Marinated Chicken Breast 200g, Caramalized Onions , Tomatoes, Chadder cheese, Ketchup aioli, Garlic aioli, Coleslaw, Lettuce, Egg.",
+    extras:"Fries"
   },
 ]
