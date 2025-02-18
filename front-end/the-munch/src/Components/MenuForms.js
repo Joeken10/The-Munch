0e714c570cfac245c0f1ac7649fa6b9219f98c2f
Joseph Kennedy <joekentafir@gmail.com>
2025-02-18 06:20:57 +0300

    creates a new db.json and modified menuforms and menuitems

--- front-end/the-munch/src/Components/MenuForms.js
+++ front-end/the-munch/src/Components/MenuForms.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MenuItems from './MenuItems';
 
 function MenuForms() {
+  const[products, setProducts] = useState([]);
+
+useEffect(()=>{
+  fetchProductData();
+})
+const fetchProductData = ()=> {
+  fetch("http://localhost:5000/menuList")
+  .then ((response) => response.json())
+  .then(data => setProducts(data));
+
+};
+// fetchProductData();
   return (
     <div>
-      
+    <MenuItems  menuProduct={products}/>
     </div>
   );
 }
--- front-end/the-munch/src/Components/MenuItems.js
+++ front-end/the-munch/src/Components/MenuItems.js
@@ -1,24 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./MenuItems.css";
 
-function MenuItems() {
+function MenuItems({ menuProduct = [] }) { 
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleDescriptionClick = (itemId) => {
+    setSelectedItem(selectedItem === itemId ? null : itemId);
+  };
+
   return (
-    <div>
-      <div className="card" style={{ width: "18rem" }}>
-        <img
-          src="/Images/Classic American Burger with Fried Egg.jpeg"
-          className="card-img-top"
-          alt="Classic Beef Burger"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Classic Beef Burger</h5>
-          <p className="card-text">
-          Accampanied with fries.
-          </p>
-          <a href="/" className="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
+    <div className="menuitem-container">
+      {menuProduct.map((item) => (
+        <ul key={item.id}>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              src={item.image} 
+              className="card-img-top"
+              alt={item.itemName}
+            />
+            <div className="card-body">
+              <h4 className="card-title">{item.itemName}</h4>
+              <h5 className="card-title">ksh{item.price}</h5>
+              
+              {/* Description Button */}
+              <button 
+                className="btn btn-primary" 
+                onClick={() => handleDescriptionClick(item.id)}
+              >
+                Description
+              </button>
+
+             
+              {selectedItem === item.id && (
+                <p className="card-text">{item.description}</p>
+              )}
+
+              
+              {Array.isArray(item.extras) && item.extras.length > 0 ? (
+                <div className="extras">
+                  <h6>Extras:</h6>
+                  <ul>
+                    {item.extras.map((extra, index) => (
+                      <li key={index}>{extra}</li>
+                    ))}
+                  </ul>
+                </div>
+              ) : (
+                <p>No extras available.</p> 
+              )}
+            </div>
+          </div>
+        </ul>
+      ))}
     </div>
-  )
+  );
 }
 
-export default MenuItems
+export default MenuItems;
